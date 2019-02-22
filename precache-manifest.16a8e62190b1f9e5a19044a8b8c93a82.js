92802607c14e79061be9d34ddba0b46cae69f8f2
ishuamali <mail2isharaa@gmail.com>
2019-02-22 17:00:35 +0530

    Updates

---
+++ precache-manifest.16a8e62190b1f9e5a19044a8b8c93a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56b995da8b16d02eae9871a793dcfe0d",
+    "url": "/Ishuamali/ballerina-message-broker/tree/broker-ui-client/static/media/message-broker.56b995da.png"
+  },
+  {
+    "revision": "d18a27a523d21d6f7c67",
+    "url": "/Ishuamali/ballerina-message-broker/tree/broker-ui-client/static/js/runtime~main.d18a27a5.js"
+  },
+  {
+    "revision": "2a03791df04b79f0e6d8",
+    "url": "/Ishuamali/ballerina-message-broker/tree/broker-ui-client/static/js/main.2a03791d.chunk.js"
+  },
+  {
+    "revision": "d47f80308a7f605b61d4",
+    "url": "/Ishuamali/ballerina-message-broker/tree/broker-ui-client/static/js/1.d47f8030.chunk.js"
+  },
+  {
+    "revision": "2a03791df04b79f0e6d8",
+    "url": "/Ishuamali/ballerina-message-broker/tree/broker-ui-client/static/css/main.a4d5a1d5.chunk.css"
+  },
+  {
+    "revision": "ce98efabe381bb65e162002fbcd1ab52",
+    "url": "/Ishuamali/ballerina-message-broker/tree/broker-ui-client/index.html"
+  }
+];
